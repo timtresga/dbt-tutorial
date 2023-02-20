5f35daa07821b5de42a5cfeb5c1a20fd35f01f08
Tim Treskatis <tim.treskatis@ginkgo-analytics.com>
2023-02-20 09:33:38 +0100

    changed whitespace removal for compiled code

--- dbt_tutorial/models/marts/core/int_orders__pivoted.sql
+++ dbt_tutorial/models/marts/core/int_orders__pivoted.sql
@@ -1,4 +1,4 @@
-{% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
 
 with payments as (
     select * from {{ ref('stg_payments') }}
@@ -8,12 +8,12 @@ pivoted as (
     select
         order_id,
 
-        {% for payment_method in payment_methods %}
+        {% for payment_method in payment_methods -%}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
         {%- if not loop.last -%}
             ,
         {%- endif -%}
-        {% endfor %}
+        {%- endfor -%}
 
     from payments
     where status = 'success'
